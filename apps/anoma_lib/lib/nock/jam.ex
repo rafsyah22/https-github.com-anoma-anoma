anoma/anoma

New Jam Implentation
New Jam implementation. Increased speed, readability, smaller output
format.

Co-authored-by: Raymond E. Pasco <raymond@heliax.dev>
Co-authored-by: mariari <mariari@protonmail.com>

apps/anoma_lib/lib/nock/jam.ex
@@ -1,120 +1,147 @@
 defmodule Nock.Jam do
-  require IEx
+  @moduledoc """
+  I am a module implementing jam serealization functionality for Nock.
+
+  ### Public API
+
+  I have the following public functionality:
+
+  - `jam/1`
+  """
+
   require Noun
-  use TypedStruct
 
-  @type cache() :: %{Noun.t() => non_neg_integer()}
+  @typedoc """
+  I am the jam cache type. I store the reference alongside with its size.
+  """
+  @type jam_cache() :: %{Noun.t() => {bitstring(), non_neg_integer()}}
 
-  typedstruct do
-    field(:position, non_neg_integer(), default: 0)
-    field(:cache, cache(), default: Map.new())
-    field(:buffer, [0 | 1], default: [])
-  end
+  @doc """
+  I am the jam function.
 
-  @spec jam(Noun.t()) :: Noun.noun_atom()
+  I serialize any Nock noun into binary format. My inverse is `cue!/1`
+  """
+
+  @spec jam(Noun.t()) :: binary()
   def jam(noun) do
-    encode(%__MODULE__{}, noun)
-    |> then(fn %__MODULE__{buffer: buf} -> buf end)
-    |> Enum.reverse()
-    |> Nock.Bits.bit_list_to_integer()
-    |> Noun.atom_integer_to_binary()
+    {bits, _cache, offset} = jam_inner(noun)
+    # sanity check
+    ^offset = bit_size(bits)
+    bits |> Nock.Cue.pad_to_binary() |> Nock.Bits.byte_order_big_to_little()
   end
 
-  @spec encode(t(), Noun.t()) :: t()
-  def encode(jam_env, noun) do
-    case fetch_cache_noun(jam_env, noun) do
-      {:ok, position} -> handle_back(jam_env, position, noun)
-      :error -> jam_env |> cache_noun(noun) |> write(noun)
-    end
-  end
-
-  @spec cache_noun(t(), Noun.t()) :: t()
-  def cache_noun(env = %__MODULE__{}, noun) do
-    %__MODULE__{
-      env
-      | cache: Map.put(env.cache, Noun.normalize_noun(noun), env.position)
-    }
-  end
-
-  @spec fetch_cache_noun(t(), Noun.t()) :: {:ok, non_neg_integer()} | :error
-  def fetch_cache_noun(env, noun) do
-    Map.fetch(env.cache, Noun.normalize_noun(noun))
-  end
-
-  @spec handle_back(t(), non_neg_integer(), Noun.t()) :: t()
-  def handle_back(env, position, noun) when Noun.is_noun_cell(noun) do
-    write_back_ref(env, position)
-  end
-
-  def handle_back(env, position, noun) when Noun.is_noun_atom(noun) do
-    # Ensure we can do arithmetic on it!
-    noun = Noun.atom_binary_to_integer(noun)
-    position_length = Nock.Bits.num_bits(position, 0)
-    atom_length = Nock.Bits.num_bits(noun, 0)
-
-    if atom_length <= position_length do
-      write(env, noun)
+  @spec jam_inner(Noun.t(), jam_cache(), non_neg_integer()) ::
+          {bitstring(), jam_cache(), non_neg_integer()}
+  defp jam_inner(noun, cache \\ %{}, offset \\ 0) do
+    # this could be Map.get_lazy if the offset tracking
+    # didn't make using that awkward.
+    with {backref, backref_size} <- Map.get(cache, noun) do
+      {backref, cache, offset + backref_size}
     else
-      write_back_ref(env, position)
+      _ ->
+        # cases where we will obviously never store a backref
+        {backref, backref_size} =
+          if offset == 0 or Noun.is_noun_zero(noun) do
+            {nil, nil}
+          else
+            # compute a backref to our offset here first
+            {offset_bits, offset_bits_size} = integer_to_bits(offset)
+
+            {encoded_offset, encoded_offset_size} =
+              jam_atom_encode(offset_bits, offset_bits_size)
+
+            backref = <<encoded_offset::bitstring, 1::1, 1::1>>
+            backref_size = encoded_offset_size + 2
+            # sanity check
+            ^backref_size = bit_size(backref)
+            {backref, backref_size}
+          end
+
+        case noun do
+          [head | tail] ->
+            {jammed_head, cache_after_head, offset_after_head} =
+              jam_inner(head, cache, offset + 2)
+
+            {jammed_tail, new_cache, new_offset} =
+              jam_inner(tail, cache_after_head, offset_after_head)
+
+            encoded_cell =
+              <<jammed_tail::bitstring, jammed_head::bitstring, 0::1, 1::1>>
+
+            encoded_cell_size = bit_size(encoded_cell)
+
+            # if the backref to here would be no larger than the encoding,
+            # put it in the cache.
+            maybe_updated_cache =
+              if backref_size <= encoded_cell_size do
+                Map.put(new_cache, noun, {backref, backref_size})
+              else
+                new_cache
+              end
+
+            {encoded_cell, maybe_updated_cache, new_offset}
+
+          zero when Noun.is_noun_zero(zero) ->
+            # there is no possible backref shorter than this,
+            # and 0 is not a valid backref offset since it means
+            # "the entire noun we are jamming". (it would be 0b111 anyway.)
+            # offset 1 would be 0b11011, 2.5x the size
+            # so no cache update. 0s are never backreffed-to
+            {<<1::1, 0::1>>, cache, offset + 2}
+
+          atom when Noun.is_noun_atom(atom) ->
+            {atom_bits, atom_size} =
+              atom
+              |> Noun.normalize_noun()
+              |> Nock.Bits.byte_order_little_to_big()
+              |> Nock.Cue.unpad_from_binary()
+
+            {encoded_atom, encoded_atom_size} =
+              jam_atom_encode(atom_bits, atom_size)
+
+            # add the atom tag bit, shadowing encoded_atom and encoded_atom_size
+            encoded_atom = <<encoded_atom::bitstring, 0::1>>
+            encoded_atom_size = encoded_atom_size + 1
+
+            # if the backref to here would be no larger than the encoding,
+            # put it in the cache.
+            maybe_updated_cache =
+              if backref_size <= encoded_atom_size do
+                Map.put(cache, noun, {backref, backref_size})
+              else
+                cache
+              end
+
+            {encoded_atom, maybe_updated_cache, offset + encoded_atom_size}
+        end
     end
   end
 
-  @spec write(t(), Noun.t()) :: t()
-  def write(env, atom) when Noun.is_noun_atom(atom) do
-    # Ensure we can do arithmetic on it!
-    atom = Noun.atom_binary_to_integer(atom)
-
-    env
-    |> write_to_env([0])
-    |> write_length(Nock.Bits.num_bits(atom, 0))
-    |> write_atom(atom)
+  @spec jam_atom_encode(<<>>, 0) :: {<<_::1>>, 1}
+  defp jam_atom_encode(<<>>, 0) do
+    {<<1::1>>, 1}
   end
 
-  def write(env, x = [hd | tl]) when Noun.is_noun_cell(x) do
-    env
-    # We reverse the tag it's [1,0] buut since we will reverse, the
-    # chunk has to be reversed
-    |> write_to_env([0, 1])
-    |> encode(hd)
-    |> encode(tl)
+  @spec jam_atom_encode(bitstring(), non_neg_integer()) ::
+          {bitstring(), non_neg_integer()}
+  defp jam_atom_encode(atom_bits, atom_size) do
+    {atom_size_as_bits, atom_size_of_size} = integer_to_bits(atom_size)
+
+    <<1::1, atom_size_truncated::bitstring>> = atom_size_as_bits
+
+    # from right to left: unary size of size,
+    # atom size with leading 1 chopped off, actual atom bits
+    encoded_atom =
+      <<atom_bits::bitstring, atom_size_truncated::bitstring, 1::1,
+        0::size(atom_size_of_size)>>
+
+    encoded_atom_size = bit_size(encoded_atom)
+
+    {encoded_atom, encoded_atom_size}
   end
 
-  @spec write_length(t(), non_neg_integer()) :: t()
-  def write_length(env, len_atom) do
-    bit_width_of_len = Nock.Bits.num_bits(len_atom, 0)
-    # We are writing [0,…ₙ₋₁,0,1] to the buffer
-    # We are encoding the length of the length of the atom that we are
-    # going to write
-    env = write_to_env(env, [1 | List.duplicate(0, bit_width_of_len)])
-
-    if bit_width_of_len == 0 do
-      env
-    else
-      # we chop off the MSB
-      # Our encoding is the LSB, buuuut since we have to write in
-      # reverse, and cut off the first
-      len_atom
-      |> Nock.Bits.integer_to_bits()
-      |> Enum.reverse()
-      |> tl()
-      |> then(&write_to_env(env, &1))
-    end
-  end
-
-  @spec write_atom(t(), Noun.noun_atom()) :: t()
-  def write_atom(env, atom) do
-    write_to_env(env, atom |> Nock.Bits.integer_to_bits() |> Enum.reverse())
-  end
-
-  @spec write_atom(t(), non_neg_integer()) :: t()
-  def write_back_ref(env, position) do
-    env
-    |> write_to_env([1, 1])
-    |> write_length(Nock.Bits.num_bits(position, 0))
-    |> write_atom(position)
-  end
-
-  defp write_to_env(env = %__MODULE__{buffer: buf, position: pos}, to_add) do
-    %__MODULE__{env | buffer: to_add ++ buf, position: pos + length(to_add)}
+  @spec integer_to_bits(non_neg_integer()) :: {bitstring(), non_neg_integer()}
+  defp integer_to_bits(n) do
+    n |> :binary.encode_unsigned(:big) |> Nock.Cue.unpad_from_binary()
   end
 end

rafsyah22/https-github.com-anoma-anoma

apps/anoma_lib/lib/nock/jam.ex
@@ -1,120 +1,147 @@
 defmodule Nock.Jam do
-  require IEx
+  @moduledoc """
+  I am a module implementing jam serealization functionality for Nock.
+
+  ### Public API
+
+  I have the following public functionality:
+
+  - `jam/1`
+  """
+
   require Noun
-  use TypedStruct
 
-  @type cache() :: %{Noun.t() => non_neg_integer()}
+  @typedoc """
+  I am the jam cache type. I store the reference alongside with its size.
+  """
+  @type jam_cache() :: %{Noun.t() => {bitstring(), non_neg_integer()}}
 
-  typedstruct do
-    field(:position, non_neg_integer(), default: 0)
-    field(:cache, cache(), default: Map.new())
-    field(:buffer, [0 | 1], default: [])
-  end
+  @doc """
+  I am the jam function.
 
-  @spec jam(Noun.t()) :: Noun.noun_atom()
+  I serialize any Nock noun into binary format. My inverse is `cue!/1`
+  """
+
+  @spec jam(Noun.t()) :: binary()
   def jam(noun) do
-    encode(%__MODULE__{}, noun)
-    |> then(fn %__MODULE__{buffer: buf} -> buf end)
-    |> Enum.reverse()
-    |> Noun.Bits.bit_list_to_integer()
-    |> Noun.atom_integer_to_binary()
+    {bits, _cache, offset} = jam_inner(noun)
+    # sanity check
+    ^offset = bit_size(bits)
+    bits |> Nock.Cue.pad_to_binary() |> Noun.Bits.byte_order_big_to_little()
   end
 
-  @spec encode(t(), Noun.t()) :: t()
-  def encode(jam_env, noun) do
-    case fetch_cache_noun(jam_env, noun) do
-      {:ok, position} -> handle_back(jam_env, position, noun)
-      :error -> jam_env |> cache_noun(noun) |> write(noun)
-    end
-  end
-
-  @spec cache_noun(t(), Noun.t()) :: t()
-  def cache_noun(env = %__MODULE__{}, noun) do
-    %__MODULE__{
-      env
-      | cache: Map.put(env.cache, Noun.normalize_noun(noun), env.position)
-    }
-  end
-
-  @spec fetch_cache_noun(t(), Noun.t()) :: {:ok, non_neg_integer()} | :error
-  def fetch_cache_noun(env, noun) do
-    Map.fetch(env.cache, Noun.normalize_noun(noun))
-  end
-
-  @spec handle_back(t(), non_neg_integer(), Noun.t()) :: t()
-  def handle_back(env, position, noun) when Noun.is_noun_cell(noun) do
-    write_back_ref(env, position)
-  end
-
-  def handle_back(env, position, noun) when Noun.is_noun_atom(noun) do
-    # Ensure we can do arithmetic on it!
-    noun = Noun.atom_binary_to_integer(noun)
-    position_length = Noun.Bits.num_bits(position, 0)
-    atom_length = Noun.Bits.num_bits(noun, 0)
-
-    if atom_length <= position_length do
-      write(env, noun)
+  @spec jam_inner(Noun.t(), jam_cache(), non_neg_integer()) ::
+          {bitstring(), jam_cache(), non_neg_integer()}
+  defp jam_inner(noun, cache \\ %{}, offset \\ 0) do
+    # this could be Map.get_lazy if the offset tracking
+    # didn't make using that awkward.
+    with {backref, backref_size} <- Map.get(cache, noun) do
+      {backref, cache, offset + backref_size}
     else
-      write_back_ref(env, position)
+      _ ->
+        # cases where we will obviously never store a backref
+        {backref, backref_size} =
+          if offset == 0 or Noun.is_noun_zero(noun) do
+            {nil, nil}
+          else
+            # compute a backref to our offset here first
+            {offset_bits, offset_bits_size} = integer_to_bits(offset)
+
+            {encoded_offset, encoded_offset_size} =
+              jam_atom_encode(offset_bits, offset_bits_size)
+
+            backref = <<encoded_offset::bitstring, 1::1, 1::1>>
+            backref_size = encoded_offset_size + 2
+            # sanity check
+            ^backref_size = bit_size(backref)
+            {backref, backref_size}
+          end
+
+        case noun do
+          [head | tail] ->
+            {jammed_head, cache_after_head, offset_after_head} =
+              jam_inner(head, cache, offset + 2)
+
+            {jammed_tail, new_cache, new_offset} =
+              jam_inner(tail, cache_after_head, offset_after_head)
+
+            encoded_cell =
+              <<jammed_tail::bitstring, jammed_head::bitstring, 0::1, 1::1>>
+
+            encoded_cell_size = bit_size(encoded_cell)
+
+            # if the backref to here would be no larger than the encoding,
+            # put it in the cache.
+            maybe_updated_cache =
+              if backref_size <= encoded_cell_size do
+                Map.put(new_cache, noun, {backref, backref_size})
+              else
+                new_cache
+              end
+
+            {encoded_cell, maybe_updated_cache, new_offset}
+
+          zero when Noun.is_noun_zero(zero) ->
+            # there is no possible backref shorter than this,
+            # and 0 is not a valid backref offset since it means
+            # "the entire noun we are jamming". (it would be 0b111 anyway.)
+            # offset 1 would be 0b11011, 2.5x the size
+            # so no cache update. 0s are never backreffed-to
+            {<<1::1, 0::1>>, cache, offset + 2}
+
+          atom when Noun.is_noun_atom(atom) ->
+            {atom_bits, atom_size} =
+              atom
+              |> Noun.normalize_noun()
+              |> Noun.Bits.byte_order_little_to_big()
+              |> Nock.Cue.unpad_from_binary()
+
+            {encoded_atom, encoded_atom_size} =
+              jam_atom_encode(atom_bits, atom_size)
+
+            # add the atom tag bit, shadowing encoded_atom and encoded_atom_size
+            encoded_atom = <<encoded_atom::bitstring, 0::1>>
+            encoded_atom_size = encoded_atom_size + 1
+
+            # if the backref to here would be no larger than the encoding,
+            # put it in the cache.
+            maybe_updated_cache =
+              if backref_size <= encoded_atom_size do
+                Map.put(cache, noun, {backref, backref_size})
+              else
+                cache
+              end
+
+            {encoded_atom, maybe_updated_cache, offset + encoded_atom_size}
+        end
     end
   end
 
-  @spec write(t(), Noun.t()) :: t()
-  def write(env, atom) when Noun.is_noun_atom(atom) do
-    # Ensure we can do arithmetic on it!
-    atom = Noun.atom_binary_to_integer(atom)
-
-    env
-    |> write_to_env([0])
-    |> write_length(Noun.Bits.num_bits(atom, 0))
-    |> write_atom(atom)
+  @spec jam_atom_encode(<<>>, 0) :: {<<_::1>>, 1}
+  defp jam_atom_encode(<<>>, 0) do
+    {<<1::1>>, 1}
   end
 
-  def write(env, x = [hd | tl]) when Noun.is_noun_cell(x) do
-    env
-    # We reverse the tag it's [1,0] buut since we will reverse, the
-    # chunk has to be reversed
-    |> write_to_env([0, 1])
-    |> encode(hd)
-    |> encode(tl)
+  @spec jam_atom_encode(bitstring(), non_neg_integer()) ::
+          {bitstring(), non_neg_integer()}
+  defp jam_atom_encode(atom_bits, atom_size) do
+    {atom_size_as_bits, atom_size_of_size} = integer_to_bits(atom_size)
+
+    <<1::1, atom_size_truncated::bitstring>> = atom_size_as_bits
+
+    # from right to left: unary size of size,
+    # atom size with leading 1 chopped off, actual atom bits
+    encoded_atom =
+      <<atom_bits::bitstring, atom_size_truncated::bitstring, 1::1,
+        0::size(atom_size_of_size)>>
+
+    encoded_atom_size = bit_size(encoded_atom)
+
+    {encoded_atom, encoded_atom_size}
   end
 
-  @spec write_length(t(), non_neg_integer()) :: t()
-  def write_length(env, len_atom) do
-    bit_width_of_len = Noun.Bits.num_bits(len_atom, 0)
-    # We are writing [0,…ₙ₋₁,0,1] to the buffer
-    # We are encoding the length of the length of the atom that we are
-    # going to write
-    env = write_to_env(env, [1 | List.duplicate(0, bit_width_of_len)])
-
-    if bit_width_of_len == 0 do
-      env
-    else
-      # we chop off the MSB
-      # Our encoding is the LSB, buuuut since we have to write in
-      # reverse, and cut off the first
-      len_atom
-      |> Noun.Bits.integer_to_bits()
-      |> Enum.reverse()
-      |> tl()
-      |> then(&write_to_env(env, &1))
-    end
-  end
-
-  @spec write_atom(t(), Noun.noun_atom()) :: t()
-  def write_atom(env, atom) do
-    write_to_env(env, atom |> Noun.Bits.integer_to_bits() |> Enum.reverse())
-  end
-
-  @spec write_atom(t(), non_neg_integer()) :: t()
-  def write_back_ref(env, position) do
-    env
-    |> write_to_env([1, 1])
-    |> write_length(Noun.Bits.num_bits(position, 0))
-    |> write_atom(position)
-  end
-
-  defp write_to_env(env = %__MODULE__{buffer: buf, position: pos}, to_add) do
-    %__MODULE__{env | buffer: to_add ++ buf, position: pos + length(to_add)}
+  @spec integer_to_bits(non_neg_integer()) :: {bitstring(), non_neg_integer()}
+  defp integer_to_bits(n) do
+    n |> :binary.encode_unsigned(:big) |> Nock.Cue.unpad_from_binary()
   end
 end