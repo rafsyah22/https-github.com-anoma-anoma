anoma/anoma

Merge Jam and Cue Modules into Noun.Jam Module
Unites Nock.Jam and Nock.Cue modules into Noun.Jam module.

apps/anoma_lib/lib/noun/jam.ex
@@ -1,14 +1,23 @@
-defmodule Nock.Cue do
+defmodule Noun.Jam do
   @moduledoc """
-  I am a module implementing cue deserialization functionality for Nock.
+  I am the implementation of Nock de/serialization functions, cue and jam.
 
   ### Public API
 
   I have the following public functionality:
 
+  - `jam/1`
   - `cue/1`
   - `cue!/1`
   """
+
+  require Noun
+
+  @typedoc """
+  I am the jam cache type. I store the reference alongside with its size.
+  """
+
+  @type jam_cache() :: %{Noun.t() => {bitstring(), non_neg_integer()}}
 
   @typedoc """
   I am the type of the cue cache, that is, I store the already decoded
@@ -16,6 +25,138 @@ defmodule Nock.Cue do
   """
 
   @type cue_cache() :: %{non_neg_integer() => Noun.t()}
+
+  ############################################################
+  #                          Jam                             #
+  ############################################################
+
+  @doc """
+  I am the jam function.
+
+  I serialize any Nock noun into binary format. My inverse is `cue!/1`
+  """
+
+  @spec jam(Noun.t()) :: binary()
+  def jam(noun) do
+    {bits, _cache, offset} = jam_inner(noun)
+    # sanity check
+    ^offset = bit_size(bits)
+    bits |> pad_to_binary() |> Nock.Bits.byte_order_big_to_little()
+  end
+
+  @spec jam_inner(Noun.t(), jam_cache(), non_neg_integer()) ::
+          {bitstring(), jam_cache(), non_neg_integer()}
+  defp jam_inner(noun, cache \\ %{}, offset \\ 0) do
+    # this could be Map.get_lazy if the offset tracking
+    # didn't make using that awkward.
+    with {backref, backref_size} <- Map.get(cache, noun) do
+      {backref, cache, offset + backref_size}
+    else
+      _ ->
+        # cases where we will obviously never store a backref
+        {backref, backref_size} =
+          if offset == 0 or Noun.is_noun_zero(noun) do
+            {nil, nil}
+          else
+            # compute a backref to our offset here first
+            {offset_bits, offset_bits_size} = integer_to_bits(offset)
+
+            {encoded_offset, encoded_offset_size} =
+              jam_atom_encode(offset_bits, offset_bits_size)
+
+            backref = <<encoded_offset::bitstring, 1::1, 1::1>>
+            backref_size = encoded_offset_size + 2
+            # sanity check
+            ^backref_size = bit_size(backref)
+            {backref, backref_size}
+          end
+
+        case noun do
+          [head | tail] ->
+            {jammed_head, cache_after_head, offset_after_head} =
+              jam_inner(head, cache, offset + 2)
+
+            {jammed_tail, new_cache, new_offset} =
+              jam_inner(tail, cache_after_head, offset_after_head)
+
+            encoded_cell =
+              <<jammed_tail::bitstring, jammed_head::bitstring, 0::1, 1::1>>
+
+            encoded_cell_size = bit_size(encoded_cell)
+
+            # if the backref to here would be no larger than the encoding,
+            # put it in the cache.
+            maybe_updated_cache =
+              if backref_size <= encoded_cell_size do
+                Map.put(new_cache, noun, {backref, backref_size})
+              else
+                new_cache
+              end
+
+            {encoded_cell, maybe_updated_cache, new_offset}
+
+          zero when Noun.is_noun_zero(zero) ->
+            # there is no possible backref shorter than this,
+            # and 0 is not a valid backref offset since it means
+            # "the entire noun we are jamming". (it would be 0b111 anyway.)
+            # offset 1 would be 0b11011, 2.5x the size
+            # so no cache update. 0s are never backreffed-to
+            {<<1::1, 0::1>>, cache, offset + 2}
+
+          atom when Noun.is_noun_atom(atom) ->
+            {atom_bits, atom_size} =
+              atom
+              |> Noun.normalize_noun()
+              |> Nock.Bits.byte_order_little_to_big()
+              |> unpad_from_binary()
+
+            {encoded_atom, encoded_atom_size} =
+              jam_atom_encode(atom_bits, atom_size)
+
+            # add the atom tag bit, shadowing encoded_atom and encoded_atom_size
+            encoded_atom = <<encoded_atom::bitstring, 0::1>>
+            encoded_atom_size = encoded_atom_size + 1
+
+            # if the backref to here would be no larger than the encoding,
+            # put it in the cache.
+            maybe_updated_cache =
+              if backref_size <= encoded_atom_size do
+                Map.put(cache, noun, {backref, backref_size})
+              else
+                cache
+              end
+
+            {encoded_atom, maybe_updated_cache, offset + encoded_atom_size}
+        end
+    end
+  end
+
+  @spec jam_atom_encode(<<>>, 0) :: {<<_::1>>, 1}
+  defp jam_atom_encode(<<>>, 0) do
+    {<<1::1>>, 1}
+  end
+
+  @spec jam_atom_encode(bitstring(), non_neg_integer()) ::
+          {bitstring(), non_neg_integer()}
+  defp jam_atom_encode(atom_bits, atom_size) do
+    {atom_size_as_bits, atom_size_of_size} = integer_to_bits(atom_size)
+
+    <<1::1, atom_size_truncated::bitstring>> = atom_size_as_bits
+
+    # from right to left: unary size of size,
+    # atom size with leading 1 chopped off, actual atom bits
+    encoded_atom =
+      <<atom_bits::bitstring, atom_size_truncated::bitstring, 1::1,
+        0::size(atom_size_of_size)>>
+
+    encoded_atom_size = bit_size(encoded_atom)
+
+    {encoded_atom, encoded_atom_size}
+  end
+
+  ############################################################
+  #                          Cue                             #
+  ############################################################
 
   @doc """
   I am the cue function.
@@ -27,9 +168,9 @@ defmodule Nock.Cue do
   """
 
   @spec cue(binary()) :: {:ok, Noun.t()} | :error
-  def cue(number) do
+  def cue(bytes) do
     try do
-      {:ok, cue!(number)}
+      {:ok, cue!(bytes)}
     rescue
       _ -> :error
     end
@@ -40,6 +181,8 @@ defmodule Nock.Cue do
 
   Given jammed input, I first change the endianness of the input, unpad the extra 0es
   inserted and proceed via the usual deserialization of Nock terms.
+
+  My inverse is `jam/1`
   """
 
   @spec cue!(binary()) :: Noun.t()
@@ -155,13 +298,13 @@ defmodule Nock.Cue do
   end
 
   @spec pad_to_binary(bitstring()) :: binary()
-  def pad_to_binary(bits) do
+  defp pad_to_binary(bits) do
     padding_bits = Kernel.rem(8 - Kernel.rem(bit_size(bits), 8), 8)
     <<0::size(padding_bits), bits::bitstring>>
   end
 
   @spec unpad_from_binary(binary()) :: {bitstring(), non_neg_integer()}
-  def unpad_from_binary(bytes) do
+  defp unpad_from_binary(bytes) do
     padded_size = bit_size(bytes)
     real_size = real_size(bytes)
 
@@ -169,6 +312,11 @@ defmodule Nock.Cue do
       bytes
 
     {bits, real_size}
+  end
+
+  @spec integer_to_bits(non_neg_integer()) :: {bitstring(), non_neg_integer()}
+  defp integer_to_bits(n) do
+    n |> :binary.encode_unsigned(:big) |> unpad_from_binary()
   end
 
   @spec count_trailing_zeros(bitstring(), non_neg_integer()) ::
@@ -184,11 +332,6 @@ defmodule Nock.Cue do
       <<rest::size(size - 1)-bitstring, 0::1>> ->
         count_trailing_zeros(rest, size - 1, acc + 1)
     end
-  end
-
-  @spec real_size(bitstring()) :: non_neg_integer()
-  defp real_size(bits) do
-    bit_size(bits) - count_leading_zeros(bits)
   end
 
   @spec count_leading_zeros(bitstring()) :: non_neg_integer()
@@ -224,5 +367,10 @@ defmodule Nock.Cue do
       <<0::1, rest::bitstring>> ->
         count_leading_zeros(rest, acc + 1)
     end
+  end
+
+  @spec real_size(bitstring()) :: non_neg_integer()
+  defp real_size(bits) do
+    bit_size(bits) - count_leading_zeros(bits)
   end
 end

rafsyah22/https-github.com-anoma-anoma

apps/anoma_lib/lib/noun/jam.ex
@@ -1,14 +1,23 @@
-defmodule Nock.Cue do
+defmodule Noun.Jam do
   @moduledoc """
-  I am a module implementing cue deserialization functionality for Nock.
+  I am the implementation of Nock de/serialization functions, cue and jam.
 
   ### Public API
 
   I have the following public functionality:
 
+  - `jam/1`
   - `cue/1`
   - `cue!/1`
   """
+
+  require Noun
+
+  @typedoc """
+  I am the jam cache type. I store the reference alongside with its size.
+  """
+
+  @type jam_cache() :: %{Noun.t() => {bitstring(), non_neg_integer()}}
 
   @typedoc """
   I am the type of the cue cache, that is, I store the already decoded
@@ -16,6 +25,138 @@ defmodule Nock.Cue do
   """
 
   @type cue_cache() :: %{non_neg_integer() => Noun.t()}
+
+  ############################################################
+  #                          Jam                             #
+  ############################################################
+
+  @doc """
+  I am the jam function.
+
+  I serialize any Nock noun into binary format. My inverse is `cue!/1`
+  """
+
+  @spec jam(Noun.t()) :: binary()
+  def jam(noun) do
+    {bits, _cache, offset} = jam_inner(noun)
+    # sanity check
+    ^offset = bit_size(bits)
+    bits |> pad_to_binary() |> Noun.Bits.byte_order_big_to_little()
+  end
+
+  @spec jam_inner(Noun.t(), jam_cache(), non_neg_integer()) ::
+          {bitstring(), jam_cache(), non_neg_integer()}
+  defp jam_inner(noun, cache \\ %{}, offset \\ 0) do
+    # this could be Map.get_lazy if the offset tracking
+    # didn't make using that awkward.
+    with {backref, backref_size} <- Map.get(cache, noun) do
+      {backref, cache, offset + backref_size}
+    else
+      _ ->
+        # cases where we will obviously never store a backref
+        {backref, backref_size} =
+          if offset == 0 or Noun.is_noun_zero(noun) do
+            {nil, nil}
+          else
+            # compute a backref to our offset here first
+            {offset_bits, offset_bits_size} = integer_to_bits(offset)
+
+            {encoded_offset, encoded_offset_size} =
+              jam_atom_encode(offset_bits, offset_bits_size)
+
+            backref = <<encoded_offset::bitstring, 1::1, 1::1>>
+            backref_size = encoded_offset_size + 2
+            # sanity check
+            ^backref_size = bit_size(backref)
+            {backref, backref_size}
+          end
+
+        case noun do
+          [head | tail] ->
+            {jammed_head, cache_after_head, offset_after_head} =
+              jam_inner(head, cache, offset + 2)
+
+            {jammed_tail, new_cache, new_offset} =
+              jam_inner(tail, cache_after_head, offset_after_head)
+
+            encoded_cell =
+              <<jammed_tail::bitstring, jammed_head::bitstring, 0::1, 1::1>>
+
+            encoded_cell_size = bit_size(encoded_cell)
+
+            # if the backref to here would be no larger than the encoding,
+            # put it in the cache.
+            maybe_updated_cache =
+              if backref_size <= encoded_cell_size do
+                Map.put(new_cache, noun, {backref, backref_size})
+              else
+                new_cache
+              end
+
+            {encoded_cell, maybe_updated_cache, new_offset}
+
+          zero when Noun.is_noun_zero(zero) ->
+            # there is no possible backref shorter than this,
+            # and 0 is not a valid backref offset since it means
+            # "the entire noun we are jamming". (it would be 0b111 anyway.)
+            # offset 1 would be 0b11011, 2.5x the size
+            # so no cache update. 0s are never backreffed-to
+            {<<1::1, 0::1>>, cache, offset + 2}
+
+          atom when Noun.is_noun_atom(atom) ->
+            {atom_bits, atom_size} =
+              atom
+              |> Noun.normalize_noun()
+              |> Noun.Bits.byte_order_little_to_big()
+              |> unpad_from_binary()
+
+            {encoded_atom, encoded_atom_size} =
+              jam_atom_encode(atom_bits, atom_size)
+
+            # add the atom tag bit, shadowing encoded_atom and encoded_atom_size
+            encoded_atom = <<encoded_atom::bitstring, 0::1>>
+            encoded_atom_size = encoded_atom_size + 1
+
+            # if the backref to here would be no larger than the encoding,
+            # put it in the cache.
+            maybe_updated_cache =
+              if backref_size <= encoded_atom_size do
+                Map.put(cache, noun, {backref, backref_size})
+              else
+                cache
+              end
+
+            {encoded_atom, maybe_updated_cache, offset + encoded_atom_size}
+        end
+    end
+  end
+
+  @spec jam_atom_encode(<<>>, 0) :: {<<_::1>>, 1}
+  defp jam_atom_encode(<<>>, 0) do
+    {<<1::1>>, 1}
+  end
+
+  @spec jam_atom_encode(bitstring(), non_neg_integer()) ::
+          {bitstring(), non_neg_integer()}
+  defp jam_atom_encode(atom_bits, atom_size) do
+    {atom_size_as_bits, atom_size_of_size} = integer_to_bits(atom_size)
+
+    <<1::1, atom_size_truncated::bitstring>> = atom_size_as_bits
+
+    # from right to left: unary size of size,
+    # atom size with leading 1 chopped off, actual atom bits
+    encoded_atom =
+      <<atom_bits::bitstring, atom_size_truncated::bitstring, 1::1,
+        0::size(atom_size_of_size)>>
+
+    encoded_atom_size = bit_size(encoded_atom)
+
+    {encoded_atom, encoded_atom_size}
+  end
+
+  ############################################################
+  #                          Cue                             #
+  ############################################################
 
   @doc """
   I am the cue function.
@@ -27,9 +168,9 @@ defmodule Nock.Cue do
   """
 
   @spec cue(binary()) :: {:ok, Noun.t()} | :error
-  def cue(number) do
+  def cue(bytes) do
     try do
-      {:ok, cue!(number)}
+      {:ok, cue!(bytes)}
     rescue
       _ -> :error
     end
@@ -40,6 +181,8 @@ defmodule Nock.Cue do
 
   Given jammed input, I first change the endianness of the input, unpad the extra 0es
   inserted and proceed via the usual deserialization of Nock terms.
+
+  My inverse is `jam/1`
   """
 
   @spec cue!(binary()) :: Noun.t()
@@ -155,13 +298,13 @@ defmodule Nock.Cue do
   end
 
   @spec pad_to_binary(bitstring()) :: binary()
-  def pad_to_binary(bits) do
+  defp pad_to_binary(bits) do
     padding_bits = Kernel.rem(8 - Kernel.rem(bit_size(bits), 8), 8)
     <<0::size(padding_bits), bits::bitstring>>
   end
 
   @spec unpad_from_binary(binary()) :: {bitstring(), non_neg_integer()}
-  def unpad_from_binary(bytes) do
+  defp unpad_from_binary(bytes) do
     padded_size = bit_size(bytes)
     real_size = real_size(bytes)
 
@@ -169,6 +312,11 @@ defmodule Nock.Cue do
       bytes
 
     {bits, real_size}
+  end
+
+  @spec integer_to_bits(non_neg_integer()) :: {bitstring(), non_neg_integer()}
+  defp integer_to_bits(n) do
+    n |> :binary.encode_unsigned(:big) |> unpad_from_binary()
   end
 
   @spec count_trailing_zeros(bitstring(), non_neg_integer()) ::
@@ -184,11 +332,6 @@ defmodule Nock.Cue do
       <<rest::size(size - 1)-bitstring, 0::1>> ->
         count_trailing_zeros(rest, size - 1, acc + 1)
     end
-  end
-
-  @spec real_size(bitstring()) :: non_neg_integer()
-  defp real_size(bits) do
-    bit_size(bits) - count_leading_zeros(bits)
   end
 
   @spec count_leading_zeros(bitstring()) :: non_neg_integer()
@@ -224,5 +367,10 @@ defmodule Nock.Cue do
       <<0::1, rest::bitstring>> ->
         count_leading_zeros(rest, acc + 1)
     end
+  end
+
+  @spec real_size(bitstring()) :: non_neg_integer()
+  defp real_size(bits) do
+    bit_size(bits) - count_leading_zeros(bits)
   end
 end